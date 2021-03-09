8e63e0c7e156b13cc201c342eee162f8a6601a72
chinmay-anand <chinmay.subscriptions@gmail.com>
2021-03-10 02:16:42 +0530

    Model implem,ented - this class maps the ui fields with the data record being updated.

--- aws_dynamo_sms/src/main/java/com/chinmay/aws_dynamo_sms/handlingformsubmission/Greeting.java
+++ aws_dynamo_sms/src/main/java/com/chinmay/aws_dynamo_sms/handlingformsubmission/Greeting.java
@@ -8,5 +8,34 @@
 package com.chinmay.aws_dynamo_sms.handlingformsubmission;
 
 public class Greeting {
+	
+	private String id;
+	private String body;
+	private String name;
+	private String title;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 }
